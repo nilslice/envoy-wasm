51f2e9b2d45b0180eff2cae605efc64b3ec49891
Steve Manuel <nilslice@gmail.com>
2020-01-02 18:48:41 -0700

    extending the rust example and SDK lib

--- api/wasm/rust/src/lib.rs
+++ api/wasm/rust/src/lib.rs
@@ -63,6 +63,118 @@ fn free(ptr: *mut u8) {
 }
 
 #[derive(Debug)]
+// #[repr(C)]
+pub enum HeaderMapType {
+    RequestHeaders,              //  = 0   // During the onLog callback these are immutable
+    RequestTrailers,             //  = 1  // During the onLog callback these are immutable
+    ResponseHeaders,             //  = 2  // During the onLog callback these are immutable
+    ResponseTrailers,            //  = 3 // During the onLog callback these are immutable
+    GrpcCreateInitialMetadata,   //  = 4
+    GrpcReceiveInitialMetadata,  //  = 5  // Immutable
+    GrpcReceiveTrailingMetadata, //  = 6 // Immutable
+    HttpCallResponseHeaders,     //  = 7     // Immutable
+    HttpCallResponseTrailers,    //  = 8    // Immutable
+                                 // Max(u32),                    // = 8,
+}
+
+impl From<u32> for HeaderMapType {
+    fn from(i: u32) -> Self {
+        match i {
+            0 => HeaderMapType::RequestHeaders,
+            1 => HeaderMapType::RequestTrailers,
+            2 => HeaderMapType::ResponseHeaders,
+            3 => HeaderMapType::ResponseTrailers,
+            4 => HeaderMapType::GrpcCreateInitialMetadata,
+            5 => HeaderMapType::GrpcReceiveInitialMetadata,
+            6 => HeaderMapType::GrpcReceiveTrailingMetadata,
+            7 => HeaderMapType::HttpCallResponseHeaders,
+            8 => HeaderMapType::HttpCallResponseTrailers,
+            // _ => HeaderMapType::Max(8), // (?)
+            _ => panic!("unknown value for HeaderMapType"),
+        }
+    }
+}
+
+impl From<HeaderMapType> for u32 {
+    fn from(htm: HeaderMapType) -> Self {
+        match htm {
+            HeaderMapType::RequestHeaders => 0,
+            HeaderMapType::RequestTrailers => 1,
+            HeaderMapType::ResponseHeaders => 2,
+            HeaderMapType::ResponseTrailers => 3,
+            HeaderMapType::GrpcCreateInitialMetadata => 4,
+            HeaderMapType::GrpcReceiveInitialMetadata => 5,
+            HeaderMapType::GrpcReceiveTrailingMetadata => 6,
+            HeaderMapType::HttpCallResponseHeaders => 7,
+            HeaderMapType::HttpCallResponseTrailers => 8,
+            // HeaderMapType::Max(_) => 8, // (?)
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum BufferType {
+    HttpRequestBody,       // = 0       // During the onLog callback these are immutable
+    HttpResponseBody,      // = 1      // During the onLog callback these are immutable
+    NetworkDownstreamData, // = 2 // During the onLog callback these are immutable
+    NetworkUpstreamData,   // = 3   // During the onLog callback these are immutable
+    HttpCallResponseBody,  // = 4  // Immutable
+    GrpcReceiveBuffer,     // = 5     // Immutable
+    Max(u32),              // = 5
+}
+
+impl From<u32> for BufferType {
+    fn from(i: u32) -> Self {
+        match i {
+            0 => BufferType::HttpRequestBody,
+            1 => BufferType::HttpResponseBody,
+            2 => BufferType::NetworkDownstreamData,
+            3 => BufferType::NetworkUpstreamData,
+            4 => BufferType::HttpCallResponseBody,
+            5 => BufferType::GrpcReceiveBuffer,
+            _ => BufferType::Max(5),
+        }
+    }
+}
+
+impl From<BufferType> for u32 {
+    fn from(bt: BufferType) -> Self {
+        match bt {
+            BufferType::HttpRequestBody => 0,
+            BufferType::HttpResponseBody => 1,
+            BufferType::NetworkDownstreamData => 2,
+            BufferType::NetworkUpstreamData => 3,
+            BufferType::HttpCallResponseBody => 4,
+            BufferType::GrpcReceiveBuffer => 5,
+            BufferType::Max(_) => 5,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum BufferFlags {
+    // These must be powers of 2.
+    EndOfStream, // = 1,
+}
+
+impl From<u32> for BufferFlags {
+    fn from(i: u32) -> Self {
+        match i {
+            1 => BufferFlags::EndOfStream,
+            _ => panic!("unknown value for BufferFlags"),
+        }
+    }
+}
+
+impl From<BufferFlags> for u32 {
+    fn from(bf: BufferFlags) -> Self {
+        match bf {
+            BufferFlags::EndOfStream => 1,
+        }
+    }
+}
+
+#[derive(Debug)]
 pub enum WasmResult {
     // Success
     Ok,
@@ -87,11 +199,11 @@ pub enum WasmResult {
     // Internal failure: trying check logs of the surrounding system.
     InternalFailure,
     // The connection/stream/pipe was broken/closed unexpectedly.
-    BrokenConnection
+    BrokenConnection,
 }
 
-impl From<i32> for WasmResult {
-    fn from(i: i32) -> Self {
+impl From<u32> for WasmResult {
+    fn from(i: u32) -> Self {
         match i {
             0 => WasmResult::Ok,
             1 => WasmResult::NotFound,
@@ -105,12 +217,12 @@ impl From<i32> for WasmResult {
             9 => WasmResult::ResultMismatch,
             10 => WasmResult::InternalFailure,
             11 => WasmResult::BrokenConnection,
-            _ => panic!("unknown value for WasmResult")
+            _ => panic!("unknown value for WasmResult"),
         }
     }
 }
 
-impl From<WasmResult> for i32 {
+impl From<WasmResult> for u32 {
     fn from(wasm: WasmResult) -> Self {
         match wasm {
             WasmResult::Ok => 0,
@@ -131,9 +243,16 @@ impl From<WasmResult> for i32 {
 
 /// Low-level Proxy-WASM APIs for the host functions.
 pub mod host {
+    use super::HeaderMapType;
+
     extern "C" {
         pub fn proxy_log(level: u32, message_data: *const u8, message_size: usize) -> u32;
-        pub fn proxy_get_header_map_pairs(p0: i32, p1:  i32, p2: i32) -> i32;
+        pub fn proxy_get_current_time_nanoseconds(nanos: &mut u64) -> u32;
+        pub fn proxy_get_header_map_pairs(
+            map_type: HeaderMapType,
+            pairs_ptr: *const u8, // (name? type?)
+            map_size: usize,      // (name?)
+        ) -> u32;
         pub fn proxy_get_property(p0: i32, p1: i32, p2: i32, p3: i32) -> i32;
         pub fn proxy_get_buffer_bytes(p0: i32, p1: i32, p2: i32, p3: i32, p4: i32) -> i32;
         pub fn proxy_replace_header_map_value(p0: i32, p1: i32, p2: i32, p3: i32, p4: i32) -> i32;
--- examples/wasm/rust-wasm/src/lib.rs
+++ examples/wasm/rust-wasm/src/lib.rs
@@ -1,25 +1,43 @@
-use proxy_wasm::{WasmResult};
+use chrono;
+#[allow(unused_imports)]
+use proxy_wasm::{host, HeaderMapType};
 
 #[no_mangle]
 fn proxy_on_queue_ready(p0: i32, p1: i32) {
-    log::debug!("rust: `proxy_on_queue_ready` called, params: p0={} p1={}", p0, p1);
+    log::debug!(
+        "rust: `proxy_on_queue_ready` called, params: p0={} p1={}",
+        p0,
+        p1
+    );
 }
 
 #[no_mangle]
 fn proxy_on_grpc_close(p0: i32, p1: i32, p2: i32) {
-    log::debug!("rust: `proxy_on_grpc_close` called, params: p0={} p1={} p2={}", p0, p1, p2);
+    log::debug!(
+        "rust: `proxy_on_grpc_close` called, params: p0={} p1={} p2={}",
+        p0,
+        p1,
+        p2
+    );
 }
 
 #[no_mangle]
 fn proxy_on_grpc_receive(p0: i32, p1: i32, p2: i32) {
-    log::debug!("rust: `proxy_on_grpc_receive` called, params: p0={} p1={} p2={}", p0, p1, p2);
+    log::debug!(
+        "rust: `proxy_on_grpc_receive` called, params: p0={} p1={} p2={}",
+        p0,
+        p1,
+        p2
+    );
 }
 
 #[no_mangle]
 fn proxy_on_grpc_receive_trailing_metadata(p0: i32, p1: i32, p2: i32) {
     log::info!(
         "rust: `proxy_on_grpc_receive_trailing_metadata` called, params: p0={} p1={} p2={}",
-        p0, p1, p2
+        p0,
+        p1,
+        p2
     );
 }
 
@@ -27,7 +45,9 @@ fn proxy_on_grpc_receive_trailing_metadata(p0: i32, p1: i32, p2: i32) {
 fn proxy_on_grpc_receive_initial_metadata(p0: i32, p1: i32, p2: i32) {
     log::info!(
         "rust: `proxy_on_grpc_receive_initial_metadata` called, params: p0={} p1={} p2={}",
-        p0, p1, p2
+        p0,
+        p1,
+        p2
     );
 }
 
@@ -35,7 +55,9 @@ fn proxy_on_grpc_receive_initial_metadata(p0: i32, p1: i32, p2: i32) {
 fn proxy_on_grpc_create_initial_metadata(p0: i32, p1: i32, p2: i32) {
     log::info!(
         "rust: `proxy_on_grpc_create_initial_metadata` called, params: p0={} p1={} p2={}",
-        p0, p1, p2
+        p0,
+        p1,
+        p2
     );
 }
 
@@ -43,7 +65,11 @@ fn proxy_on_grpc_create_initial_metadata(p0: i32, p1: i32, p2: i32) {
 fn proxy_on_http_call_response(p0: i32, p1: i32, p2: i32, p3: i32, p4: i32) {
     log::info!(
         "rust: `proxy_on_http_call_response` calledcalled, params: p0={} p1={} p2={} p3={} p4={}",
-        p0, p1, p2, p3, p4
+        p0,
+        p1,
+        p2,
+        p3,
+        p4
     );
 }
 
@@ -60,92 +86,149 @@ fn proxy_on_log(p0: i32) {
 #[no_mangle]
 fn proxy_on_done(p0: i32) -> i32 {
     log::debug!("rust: `proxy_on_done` called, params: p0={}", p0);
-    1
+    0
 }
 
 #[no_mangle]
 fn proxy_on_response_trailers(p0: i32, p1: i32) -> i32 {
-    log::debug!("rust: `proxy_on_response_trailers` called, params: p0={} p1={}", p0, p1);
-    1
+    log::debug!(
+        "rust: `proxy_on_response_trailers` called, params: p0={} p1={}",
+        p0,
+        p1
+    );
+    0
 }
 
 #[no_mangle]
 fn proxy_on_response_body(p0: i32, p1: i32, p2: i32) -> i32 {
-    log::debug!("rust: `proxy_on_response_body` called, params: p0={} p1={} p2={}", p0, p1, p2);
-    1
+    log::debug!(
+        "rust: `proxy_on_response_body` called, params: p0={} p1={} p2={}",
+        p0,
+        p1,
+        p2
+    );
+    0
 }
 
 #[no_mangle]
 fn proxy_on_response_metadata(p0: i32, p1: i32) -> i32 {
-    log::debug!("rust: `proxy_on_response_metadata` called, params: p0={} p1={}", p0, p1);
-    1
+    log::debug!(
+        "rust: `proxy_on_response_metadata` called, params: p0={} p1={}",
+        p0,
+        p1
+    );
+    0
 }
 
 #[no_mangle]
 fn proxy_on_response_headers(p0: i32, p1: i32) -> i32 {
-    log::debug!("rust: `proxy_on_response_headers` called, params: p0={} p1={}", p0, p1);
-    1
+    log::debug!(
+        "rust: `proxy_on_response_headers` called, params: p0={} p1={}",
+        p0,
+        p1
+    );
+    0
 }
 
 #[no_mangle]
 fn proxy_on_request_trailers(p0: i32, p1: i32) -> i32 {
-    log::debug!("rust: `proxy_on_request_trailers` called, params: p0={} p1={}", p0, p1);
-    1
+    log::debug!(
+        "rust: `proxy_on_request_trailers` called, params: p0={} p1={}",
+        p0,
+        p1
+    );
+    0
 }
 
 #[no_mangle]
 fn proxy_on_request_body(p0: i32, p1: i32, p2: i32) -> i32 {
-    log::debug!("rust: `proxy_on_request_body` called, params: p0={} p1={} p2={}", p0, p1, p2);
-    1
+    log::debug!(
+        "rust: `proxy_on_request_body` called, params: p0={} p1={} p2={}",
+        p0,
+        p1,
+        p2
+    );
+    0
 }
 
 #[no_mangle]
 fn proxy_on_request_metadata(p0: i32, p1: i32) -> i32 {
-    log::debug!("rust: `proxy_on_request_metadata` called, params: p0={} p1={}", p0, p1);
-    1
+    log::debug!(
+        "rust: `proxy_on_request_metadata` called, params: p0={} p1={}",
+        p0,
+        p1
+    );
+    0
 }
 
 #[no_mangle]
-fn proxy_on_request_headers(p0: i32, p1: i32) -> i32 {
+fn proxy_on_request_headers(p0: u32, p1: u32) -> u32 {
     unsafe {
-        let hdr = proxy_wasm::host::proxy_get_header_map_pairs(p0, p1, 1);
-        log::info!("rust headers: {:?}", hdr);
+        let data: *const u8 = "".as_ptr();
+        host::proxy_get_header_map_pairs(HeaderMapType::RequestHeaders, data, 1024);
+        log::debug!("rust headers: {:?} !!!!!!!!", *data);
     }
-    log::debug!("rust: `proxy_on_request_headers` called, params: p0={} p1={}", p0, p1);
-    1
+    log::debug!(
+        "rust: `proxy_on_request_headers` called, params: p0={} p1={}",
+        p0,
+        p1
+    );
+    0
 }
 
 #[no_mangle]
 fn proxy_on_upstream_connection_close(p0: i32, p1: i32) {
-    log::debug!("rust: `proxy_on_upstream_connection_close` called, params: p0={} p1={}", p0, p1);
+    log::debug!(
+        "rust: `proxy_on_upstream_connection_close` called, params: p0={} p1={}",
+        p0,
+        p1
+    );
 }
 
 #[no_mangle]
 fn proxy_on_downstream_connection_close(p0: i32, p1: i32) {
-    log::debug!("rust: `proxy_on_downstream_connection_close` called, params: p0={} p1={}", p0, p1);
+    log::debug!(
+        "rust: `proxy_on_downstream_connection_close` called, params: p0={} p1={}",
+        p0,
+        p1
+    );
 }
 
 #[no_mangle]
 fn proxy_on_upstream_data(p0: i32, p1: i32, p2: i32) -> i32 {
-    log::debug!("rust: `proxy_on_upstream_data` called, params: p0={} p1={} p2={}", p0, p1, p2);
-    1
+    log::debug!(
+        "rust: `proxy_on_upstream_data` called, params: p0={} p1={} p2={}",
+        p0,
+        p1,
+        p2
+    );
+    0
 }
 
 #[no_mangle]
 fn proxy_on_downstream_data(p0: i32, p1: i32, p2: i32) -> i32 {
-    log::debug!("rust: `proxy_on_downstream_data` called, params: p0={} p1={} p2={}", p0, p1, p2);
-    1
+    log::debug!(
+        "rust: `proxy_on_downstream_data` called, params: p0={} p1={} p2={}",
+        p0,
+        p1,
+        p2
+    );
+    0
 }
 
 #[no_mangle]
 fn proxy_on_new_connection(p0: i32) -> i32 {
     log::debug!("rust: `proxy_on_new_connection` called, params: p0={}", p0);
-    1
+    0
 }
 
 #[no_mangle]
 fn proxy_on_create(p0: i32, p1: i32) {
-    log::debug!("rust: `proxy_on_create` called, params: p0={} p1={}", p0, p1);
+    log::debug!(
+        "rust: `proxy_on_create` called, params: p0={} p1={}",
+        p0,
+        p1
+    );
 }
 
 #[no_mangle]
@@ -155,12 +238,24 @@ fn proxy_on_tick(p0: i32) {
 
 #[no_mangle]
 fn proxy_on_configure(p0: i32, p1: i32) -> i32 {
-    log::debug!("rust: `proxy_on_configure` called, params: p0={} p1={}", p0, p1);
+    log::debug!(
+        "rust: `proxy_on_configure` called, params: p0={} p1={}",
+        p0,
+        p1
+    );
+    unsafe {
+        let mut nanos = 0u64;
+        let _result = host::proxy_get_current_time_nanoseconds(&mut nanos);
+
+        use chrono::TimeZone;
+        let time = chrono::Utc.timestamp_nanos(nanos as i64);
+        log::debug!("rust timestamp: {}", time);
+    }
     1
 }
 
 #[no_mangle]
 fn proxy_on_start(p0: i32, p1: i32) -> i32 {
     log::debug!("rust: `proxy_on_start` called, params: p0={} p1={}", p0, p1);
-    1
+    0
 }
